0229975e5bbf80c1be0c501923e50425c1bccd16
alexdavidoff <ad35@pdx.edu>
2019-06-03 14:02:32 -0700

    Attempt to fix multithreading functionality

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        ClientGUI app = new ClientGUI("localhost", 2222);
+        ClientGUI app = new ClientGUI("localhost", 5000);
     }
 }
